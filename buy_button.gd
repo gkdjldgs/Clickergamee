aabe7e756e3c4d44113ac5172aae17be334543be
melon <jermaise@gmail.com>
2025-10-22 20:21:56 -0600

    Shopping
    Etting up the framework for 4 ish power ups that I want to add to the game, gotta keep it simple

--- buy_button.gd
+++ buy_button.gd
@@ -13,7 +13,8 @@ func _process(delta: float) -> void:
 
 func _on_pressed() -> void:
 	if autoload.money >= autoload.limit:
-		autoload.money = autoload.money - 20
+		autoload.money = autoload.money - autoload.limit
+		autoload.multi = autoload.multi + 0.5
 		autoload.limit = autoload.limit + 10 * 1.5
 		print(autoload.limit)
 		
