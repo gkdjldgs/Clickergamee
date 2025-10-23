f68ac0edb8e5be0bb4c0e6aa2ba85fc33bd2885d
melon <jermaise@gmail.com>
2025-10-22 18:35:15 -0600

    Script bugging out
    My script was bugging out so I hade to delete and make one anew. Still working on the shop menu thou.

--- shopm.gd
+++ shopm.gd
@@ -1,6 +1,5 @@
 extends Node2D
-var money = 0
-var multi = 1
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,7 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file('res://shopmenu.tscn')
