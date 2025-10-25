d725b6342147ea3ef9283b8e45d1e72da3be9198
melon <jermaise@gmail.com>
2025-10-25 10:28:44 -0600

    Errors with upgrade 2
    So basically I'm implementing the spawning function where based on the number of  syrup refineries one will spawn in the main scene
    but there are alot of bugs with it i have to trouble shoot

--- main.gd
+++ main.gd
@@ -7,10 +7,28 @@ var limit1 = 2
 var amount2 = 0
 var limit2 = 50
 var amount3 = 0
+#code for spawning the syrup things on the screen.
+@onready var syrupref = preload("res://syrupref.tscn")
+var permposition = Vector2(0,0)
+func syrupspawn():
+	var permposition = Vector2(permposition.x,permposition.y)
+	permposition.y = 0
+	permposition.x = 0
+	var syrup = syrupref.instantiate()
+	for number in range(amount2):
+		if permposition.x < 0:
+			permposition.x = 70
+		elif permposition.x >= 70 and permposition.x <=630:
+			permposition.x += 70
+		elif permposition.x >= 630:
+			permposition.x = 0
+			permposition.y -= 100
+		syrup.global_position = Vector2(permposition.x, permposition.y)
+		add_child(syrup)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for number in range(amount2):
-		pass
+	syrupspawn()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
