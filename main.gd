396efa4ad711eb05bbfe191f897879845e3e6c9e
melon <jermaise@gmail.com>
2025-10-26 17:30:58 -0600

    CODING IS HARD
    I'm once again trouble shooting my game, its really painful, I'm trying to make the children I'm making reappear when loading the main screen but dissappear when on the shop screen, but I'll try something else.

--- main.gd
+++ main.gd
@@ -4,14 +4,14 @@ var money = 0
 var multi = 1
 var amount1 = 0
 var limit1 = 2
-var amount2 = 2
+var amount2 = 20
 var limit2 = 50
 var amount3 = 0
 #code for spawning the syrup things on the screen.
 var syr = amount2 + 1
 
 var check = 1
-var permy = 500
+var permy = 300
 var permx = 0
 var permposition = Vector2(permy,permx)
 func syrupspawn():
@@ -21,22 +21,21 @@ func syrupspawn():
 		print(number)
 		var syrup = syrupref.instantiate()
 		if permx >= 500:
-			permx = 100
-			permy += 30
+			permx = randi_range(25,50)
+			permy += randi_range(35,45)
 		elif permx >= 0 and permx <=500:
-			permx += 50
+			permx += 80
 		elif permx <= 0:
 			permx = 200
 		print(number)
 		add_child(syrup)
 		syrup.position = Vector2(permx, permy)
-		if number == 0:
-			check = 0
-			
+
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	syrupspawn()
+	if check == 1:
+		syrupspawn()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -45,3 +44,4 @@ func _process(delta: float) -> void:
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file('res://shopm.tscn')
 	check = 0
+	print(check)
